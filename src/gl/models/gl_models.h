f8006bfd9ec839e77c39bb63aa202dd37982fc48
Christoph Oelckers <coelckers@users.noreply.github.com>
2014-06-19 15:22:00 +0200

    - initialize model data at engine start, not at level start.

--- src/gl/models/gl_models.h
+++ src/gl/models/gl_models.h
@@ -352,7 +352,6 @@ struct FSpriteModelFrame
 
 class GLSprite;
 
-void gl_InitModels();
 FSpriteModelFrame * gl_FindModelFrame(const PClass * ti, int sprite, int frame, bool dropped);
 
 void gl_RenderModel(GLSprite * spr, int cm);
