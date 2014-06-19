8c8b9df4188c613dc31b98fee59b6c408a4f6cb7
Christoph Oelckers <coelckers@users.noreply.github.com>
2014-06-19 15:22:00 +0200

    - initialize model data at engine start, not at level start.

--- src/r_data/sprites.cpp
+++ src/r_data/sprites.cpp
@@ -15,6 +15,8 @@
 #include "r_data/voxels.h"
 #include "textures/textures.h"
 
+void gl_InitModels();
+
 // variables used to look up
 //	and range check thing_t sprites patches
 TArray<spritedef_t> sprites;
@@ -1269,6 +1271,8 @@ void R_InitSprites ()
 
 	// [RH] Sort the skins, but leave base as skin 0
 	//qsort (&skins[PlayerClasses.Size ()], skins.Size()-PlayerClasses.Size (), sizeof(FPlayerSkin), skinsorter);
+
+	gl_InitModels();
 }
 
 void R_DeinitSpriteData()
