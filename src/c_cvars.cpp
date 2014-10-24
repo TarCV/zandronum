8d42f3d414252479ac7a92b44487e36491800460
Christoph Oelckers <coelckers@users.noreply.github.com>
2014-10-25 00:01:04 +0200

    - fixed some bad logic operations.

--- src/c_cvars.cpp
+++ src/c_cvars.cpp
@@ -539,9 +539,10 @@ UCVarValue FBaseCVar::FromString (const char *value, ECVarType type)
 					goodv = false;
 				break;
 			default:
-				if (value[i] < '0' && value[i] > '9' &&
-					value[i] < 'A' && value[i] > 'F' &&
-					value[i] < 'a' && value[i] > 'f')
+				if (value[i] < '0' || 
+					(value[i] > '9' && value[i] < 'A') || 
+					(value[i] > 'F' && value[i] < 'a') || 
+					value[i] > 'f')
 				{
 					goodv = false;
 				}
