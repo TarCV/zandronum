67598f6ad400fad5233483ea8ebd021c3742891b
Benjamin Berkels <23320065+TorrSamaho@users.noreply.github.com>
2015-08-01 21:26:59 +0200

    Merged with GZDoom Git commit 5e96b11f428c6276bde110111b791f325db6de18

--- src/sdl/iwadpicker_cocoa.mm
+++ src/sdl/iwadpicker_cocoa.mm
@@ -159,7 +159,7 @@ static const char* const tableHeaders[NUM_COLUMNS] = { "IWAD", "Game" };
 	cancelled = false;
 
 	app = [NSApplication sharedApplication];
-	id windowTitle = [NSString stringWithUTF8String:GAMESIG " " DOTVERSIONSTR ": Select an IWAD to use"];
+	id windowTitle = [NSString stringWithFormat:@"%s %s", GAMESIG, GetVersionString()];
 
 	NSRect frame = NSMakeRect(0, 0, 440, 450);
 	window = [[NSWindow alloc] initWithContentRect:frame styleMask:NSTitledWindowMask backing:NSBackingStoreBuffered defer:NO];
