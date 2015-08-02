3758b33c8ad0771f792f2dd27388484bca7fd4b6
Benjamin Berkels <23320065+TorrSamaho@users.noreply.github.com>
2015-08-02 17:57:33 +0200

    Merged with G1.9pre-314-gac0c07a

--- src/gl/models/gl_models.h
+++ src/gl/models/gl_models.h
@@ -94,17 +94,6 @@ protected:
 		char            name[16];
 		FModelVertex *vertices;
 		FModelVertex *normals;
-
-		ModelFrame()
-		{
-			vertices = normals = NULL;
-		}
-
-		~ModelFrame()
-		{
-			if (vertices) delete [] vertices;
-			if (normals) delete [] normals;
-		}
 	};
 
 	struct DMDLoDInfo
@@ -212,7 +201,6 @@ class FMD3Model : public FModel
 			if (tris) delete [] tris;
 			if (vertices) delete [] vertices;
 			if (texcoords) delete [] texcoords;
-			for (int i=0;i<numSkins;i++) delete skins[i];
 			if (skins) delete [] skins;
 		}
 	};
