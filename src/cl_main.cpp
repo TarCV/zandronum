e1ce6e56647288527bb433a6fa153b31f5ef9d26
Teemu Piippo <crimsondusk64@gmail.com>
2015-09-05 17:25:21 +0300

    Fixed: turbo cheat didn't work online (fixes 2390)

--- src/cl_main.cpp
+++ src/cl_main.cpp
@@ -8137,7 +8137,9 @@ static void client_SetGameModeLimits( BYTESTREAM_s *pByteStream )
 	Value.Int = NETWORK_ReadByte( pByteStream );
 	sv_cheats.ForceSet( Value, CVAR_Int );
 	// [BB] This ensures that am_cheat respects the sv_cheats value we just set.
-	am_cheat = am_cheat;
+	am_cheat.Callback();
+	// [TP] Same for turbo
+	turbo.Callback();
 
 	// Read in, and set the value for sv_fastweapons.
 	Value.Int = NETWORK_ReadByte( pByteStream );
