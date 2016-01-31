bf9882c955a0df339150749a10d58512863da381
Benjamin Berkels <23320065+TorrSamaho@users.noreply.github.com>
2016-01-31 21:23:32 +0100

    The ZDoom jumping physics from ZDoom SVN revision 2790 are now used. The old jumping behavior known from Skulltag can be restored with the new CVAR compat_skulltagjumping (addresses 171).

--- src/d_main.cpp
+++ src/d_main.cpp
@@ -799,6 +799,7 @@ CVAR (Flag, compat_oldzdoomzmovement, zacompatflags, ZACOMPATF_OLD_ZDOOM_ZMOVEME
 CVAR (Flag, compat_fullweaponlower,		zacompatflags, ZACOMPATF_FULL_WEAPON_LOWER);
 CVAR (Flag, compat_autoaim,		zacompatflags, ZACOMPATF_AUTOAIM);
 CVAR (Flag, compat_silentwestspawns,	zacompatflags, ZACOMPATF_SILENT_WEST_SPAWNS);
+CVAR (Flag, compat_skulltagjumping,	zacompatflags, ZACOMPATF_SKULLTAG_JUMPING);
 
 //==========================================================================
 //
