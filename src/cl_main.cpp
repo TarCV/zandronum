496e78bd89633b1a5702a9f81cd44db139e67657
Edoardo Prezioso <edward.san.dev@gmail.com>
2016-04-17 23:26:48 +0200

    Fixed: clients were not informed correctly about the morphed player's morph style (fixes 2621).

--- src/cl_main.cpp
+++ src/cl_main.cpp
@@ -3960,6 +3960,7 @@ static void client_SpawnPlayer( BYTESTREAM_s *pByteStream, bool bMorph )
 	bool			bWasWatchingPlayer;
 	AActor			*pCameraActor;
 	APlayerPawn		*pOldActor;
+	int				MorphStyle;
 	USHORT			usActorNetworkIndex = 0;
 	const PClass	*pType;
 
@@ -3997,6 +3998,9 @@ static void client_SpawnPlayer( BYTESTREAM_s *pByteStream, bool bMorph )
 
 	if ( bMorph )
 	{
+		// [EP] Read in the morph style.
+		MorphStyle = NETWORK_ReadShort( pByteStream );
+
 		// [BB] Read in the identification of the morphed playerpawn
 		usActorNetworkIndex = NETWORK_ReadShort( pByteStream );
 	}
@@ -4317,14 +4321,20 @@ static void client_SpawnPlayer( BYTESTREAM_s *pByteStream, bool bMorph )
 		pPlayer->morphTics = -1;
 		// [EP] Still, assign the current class pointer, because it's used by the status bar
 		pPlayer->MorphedPlayerClass = pType;
+		// [EP] Set the morph style, too.
+		pPlayer->MorphStyle = MorphStyle;
 	}
 	else
 	{
 		pPlayer->morphTics = 0;
 
 		// [BB] If the player was just unmorphed, we need to set reactiontime to the same value P_UndoPlayerMorph uses.
+		// [EP] The morph style, too.
 		if ( bPlayerWasMorphed )
+		{
 			pPlayer->mo->reactiontime = 18;
+			pPlayer->MorphStyle = 0;
+		}
 	}
 
 
