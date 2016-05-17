8d1ce7b5bff117bcc4174615c73f6a9203a88ea8
Benjamin Berkels <torr.samaho@quantentunnel.de>
2016-05-17 21:06:55 +0200

    Backed out changeset: cef36570caa4 (fixes regression reported in 2338)

--- src/thingdef/thingdef_codeptr.cpp
+++ src/thingdef/thingdef_codeptr.cpp
@@ -690,19 +690,18 @@ DEFINE_ACTION_FUNCTION_PARAMS(AActor, A_Jump)
 	ACTION_PARAM_INT(count, 0);
 	ACTION_PARAM_INT(maxchance, 1);
 
-	// [TP] Heuristic: if we will certainly do the jump and we only have one possible jump target,
-	// we know exactly where state flow is going, so we can do the jump on our own as the client.
-	bool predictable = ( maxchance >= 256 ) && ( count == 2 );
-
 	// [BC] Don't jump here in client mode.
-	if ( NETWORK_InClientModeAndActorNotClientHandled( self ) && ( predictable == false ) )
-		return;
+	if ( NETWORK_InClientMode() )
+	{
+		if (( self->ulNetworkFlags & NETFL_CLIENTSIDEONLY ) == false )
+			return;
+	}
 
 	if (count >= 2 && (maxchance >= 256 || pr_cajump() < maxchance))
 	{
 		int jumps = 2 + (count == 2? 0 : (pr_cajump() % (count - 1)));
 		ACTION_PARAM_STATE(jumpto, jumps);
-		ACTION_JUMP(jumpto, ( predictable == false ) ? CLIENTUPDATE_FRAME : ClientJumpUpdateFlags::FromInt ( 0 ) ); // [BC] Random state changes shouldn't be client-side.
+		ACTION_JUMP(jumpto, CLIENTUPDATE_FRAME ); // [BC] Random state changes shouldn't be client-side.
 	}
 	ACTION_SET_RESULT(false);	// Jumps should never set the result for inventory state chains!
 }
