17f0f23aff2ea117260695482898199d970c1ba7
Benjamin Berkels <torr.samaho@quantentunnel.de>
2016-09-21 21:25:36 +0200

    CLIENT_SpawnThing now can warn when spawning fails.

--- src/cl_main.cpp
+++ src/cl_main.cpp
@@ -2434,6 +2434,8 @@ AActor *CLIENT_SpawnThing( const PClass *pType, fixed_t X, fixed_t Y, fixed_t Z,
 		if ( invasion )
 			pActor->ulInvasionWave = INVASION_GetCurrentWave( );
 	}
+	else
+		CLIENT_PrintWarning( "CLIENT_SpawnThing: Failed to spawn actor %s with id %ld\n", lNetID, pType->TypeName.GetChars( ) );
 
 	return ( pActor );
 }
