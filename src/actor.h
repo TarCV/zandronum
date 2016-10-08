487d0621daf9b9d2360761ac4c0c4298220e578b
Benjamin Berkels <23320065+TorrSamaho@users.noreply.github.com>
2016-10-08 14:27:42 +0200

    added a server warning to IDList::useID

--- src/actor.h
+++ src/actor.h
@@ -1371,14 +1371,7 @@ public:
 		clear ( );
 	}
 
-	void useID ( const LONG lNetID, T *pActor )
-	{
-		if ( isIndexValid ( lNetID ) )
-		{
-			_entries[lNetID].bFree = false;
-			_entries[lNetID].pActor = pActor;
-		}
-	}
+	void useID ( const LONG lNetID, T *pActor );
 
 	void freeID ( const LONG lNetID )
 	{
