b12c808153bf7d31180c37eb0831d88ad331971a
Benjamin Berkels <torr.samaho@quantentunnel.de>
2016-10-09 12:25:00 +0200

    Fixed: The client ignored the value of the CVAR telezoom online (fixes 2868).

--- src/sv_commands.cpp
+++ src/sv_commands.cpp
@@ -1695,7 +1695,7 @@ void SERVERCOMMANDS_TeleportThing( AActor *pActor, bool bSourceFog, bool bDestFo
 	command.SetAngle( pActor->angle );
 	command.SetSourcefog( bSourceFog );
 	command.SetDestfog( bDestFog );
-	command.SetTelezoom( bTeleZoom );
+	command.SetTeleportzoom( bTeleZoom );
 	command.sendCommandToClients( ulPlayerExtra, flags );
 }
 
