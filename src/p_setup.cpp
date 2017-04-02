744b37588cfea0fba2dbd8f9c242531de838434a
Benjamin Berkels <23320065+TorrSamaho@users.noreply.github.com>
2017-04-02 10:38:43 +0200

    The memory required for the bot nodes is now dynamically allocated when the first bot is added and freed when the last bot is removed. Thus, the CVAR sv_disallowbots is obsolete and was removed (adapted form Konar6's kpatch).

--- src/p_setup.cpp
+++ src/p_setup.cpp
@@ -3174,12 +3174,16 @@ void P_LoadBlockMap (MapData * map)
 	blockmap = blockmaplump+4;
 
 	// [BC] Also, build the node list for the bot pathing module.
+	// [K6] For the server, this is handled in CSkullBot(), unless we already have bots in game (from the previous map).
 	if (( NETWORK_InClientMode() == false ) &&
 		(( level.flagsZA & LEVEL_ZA_NOBOTNODES ) == false ) &&
-		(( NETWORK_GetState( ) != NETSTATE_SERVER ) || ( sv_disallowbots == false )))
+		(( NETWORK_GetState( ) != NETSTATE_SERVER ) || ( BOTS_CountBots( ) > 0 )))
 	{
 		ASTAR_BuildNodes( );
 	}
+
+	if ( level.flagsZA & LEVEL_ZA_NOBOTNODES || level.flagsZA & LEVEL_ZA_ISLOBBY )
+		BOTS_RemoveAllBots( false );
 }
 
 
