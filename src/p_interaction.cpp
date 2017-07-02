511a32e0f03e6dca6a1bf09ed36c4fdfe3720d72
Ru5tK1ng <Ru5tK1ng@localhost>
2017-07-02 17:28:23 -0500

    Frozen players were still frozen if they spectated (Fixes 3165).

--- src/p_interaction.cpp
+++ src/p_interaction.cpp
@@ -2693,6 +2693,9 @@ void PLAYER_SetDefaultSpectatorValues( player_t *pPlayer )
 	pPlayer->mo->flags5 = 0;
 	pPlayer->mo->RenderStyle = STYLE_None;
 
+	// [RK] Clear the frozen flags so the spectator can move.
+	pPlayer->cheats &= ~(CF_FROZEN | CF_TOTALLYFROZEN);
+
 	// [BB] Speed and viewheight of spectators should be independent of the player class.
 	pPlayer->mo->Speed = FRACUNIT;
 	pPlayer->mo->ForwardMove1 = pPlayer->mo->ForwardMove2 = FRACUNIT;
